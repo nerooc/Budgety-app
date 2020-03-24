f70504299c027dd4a86a37e57bd7a206b91f8688
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-03-24 21:29:33 +0100

    adding init func

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 //BUDGET CONTROLLER
 var budgetController = (function() {
-	//code
+	var Expense = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var Income = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 })();
 
 //UI CONTROLLER
@@ -31,7 +41,18 @@ var UIController = (function() {
 
 //GLOBAL APP CONTROLER
 var controller = (function(budgetCtrl, UICtrl) {
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function() {
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
+
 	var ctrlAddItem = function() {
 		var input = UICtrl.getInput();
 		console.log(input);
@@ -42,11 +63,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 		//5. Display the budget on the UI
 	};
 
-	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-	document.addEventListener('keypress', function(event) {
-		if (event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
-		}
-	});
+	return {
+		init: function() {
+			console.log('Application has started.');
+			setupEventListeners();
+		},
+	};
 })(budgetController, UIController);
+
+controller.init();
