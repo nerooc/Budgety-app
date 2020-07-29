bf2f547887b6ddd63503a515aa75b844beefddd3
Tomasz Gajda <tomasz_gajda@outlook.com>
2020-07-29 23:47:14 +0200

    added budget controller mechanic calculating everything we need

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ var budgetController = (function() {
 		this.value = value;
 	};
 
+	var calculateTotal = function(type){
+		var sum = 0;
+		data.allItems[type].forEach(function(cur){
+			sum += cur.value;
+		});
+
+		data.totals[type] = sum;
+	};
+
 	var data = {
 		allItems: {
 			exp: [],
@@ -22,6 +31,9 @@ var budgetController = (function() {
 			exp: 0,
 			inc: 0
 		},
+
+		budget: 0,
+		percentage: -1
 	};
 
 	return {
@@ -47,6 +59,35 @@ var budgetController = (function() {
 			//return the new element
 			return newItem;
 		},
+
+		calculateBudget: function(){
+			//calculate total income and expenses
+			calculateTotal('exp');
+			calculateTotal('inc');
+
+			//calculate the budget: income - expenses
+			data.budget = data.totals.inc - data.totals.exp;
+
+			//calculate the percentage of income that we spent
+			if(data.totals.inc > 0){
+				data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+			} else {
+				data.percentage = -1;
+			}
+		},
+
+		getBudget: function(){
+			return{
+				budget: data.budget,
+				totalInc: data.totals.inc,
+				totalExp: data.totals.exp,
+				percentage: data.percentage
+			};
+		},
+		
+		testing: function(){
+			console.log(data);
+		}
 
 	};
 })();
@@ -132,10 +173,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 	var updateBudget = function(){
 		//1. Calculate the budget
-
+		budgetCtrl.calculateBudget();
 		//2. Return the budget
-
+		var budget = budgetCtrl.getBudget();
 		//3. Display the budget on the UI
+		console.log(budget);
 	};
 
 	var ctrlAddItem = function() {
