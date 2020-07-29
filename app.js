a44eecdb1332936f92eb5f41212d41b7ba467299
Tomasz Gajda <tomasz_gajda@outlook.com>
2020-07-29 16:14:53 +0200

    UI item adding mechanic added

--- app.js
+++ app.js
@@ -58,6 +58,8 @@ var UIController = (function() {
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: '.expenses__list'
 	};
 	
 	return {
@@ -69,6 +71,29 @@ var UIController = (function() {
 
 				value: document.querySelector(DOMstrings.inputValue).value,
 			};
+		},
+
+		addListItem: function(obj, type){
+			// create htnl string with placeholder text
+			var html, newHtml, element;
+
+			if(type == 'inc'){
+				element = DOMstrings.incomeContainer;
+				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+			} else if (type === 'exp'){
+				element = DOMstrings.expensesContainer;
+				html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div> </div></div>'
+			}
+
+			// replace placeholder text with some actual data
+			newHtml = html.replace('%id%', obj.id);
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
+
+			// insert html into dom
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+	
+
 		},
 
 		getDOMstrings: function() {
@@ -98,6 +123,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
+		UICtrl.addListItem(newItem, input.type);
 		//1. Get the field input data
 		//2. Add the item to the budget controller
 		//3. Add the item to the UI
