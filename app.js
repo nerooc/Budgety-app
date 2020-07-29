74c515e14367d609bda5a919d53b698ec830df5d
Tomasz Gajda <tomasz_gajda@outlook.com>
2020-07-29 16:38:53 +0200

    input values clearing after new income/expense is added

--- app.js
+++ app.js
@@ -96,6 +96,19 @@ var UIController = (function() {
 
 		},
 
+		clearFields: function(){
+			var fields, fieldsArr;
+			
+			fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+			fieldsArr = Array.prototype.slice.call(fields);
+
+			fieldsArr.forEach(function(current, index, array){
+				current.value = "";
+			});
+
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		},
@@ -124,9 +137,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
 		UICtrl.addListItem(newItem, input.type);
-		//1. Get the field input data
-		//2. Add the item to the budget controller
-		//3. Add the item to the UI
+		UICtrl.clearFields();
+	
 		//4. Calculate the budget
 		//5. Display the budget on the UI
 	};
