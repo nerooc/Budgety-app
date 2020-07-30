8ba2071c4af61b2d37e82134fb6af6c8b5a646d4
Tomasz Gajda <tomasz_gajda@outlook.com>
2020-07-30 12:56:28 +0200

    calculating percentages in budget controller

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ var budgetController = (function() {
 		this.id = id;
 		this.description = description;
 		this.value = value;
+		this.percentage = -1;
+	};
+
+	Expense.prototype.calcPercentage = function(totalIncome){
+		if(totalIncome > 0){
+			this.percentage = Math.round((this.value/totalIncome) * 100);
+		} else {
+			this.percentage = -1;
+		}
+	};
+
+	Expense.prototype.getPercentage = function(){
+		return this.percentage;
 	};
 
 	var Income = function(id, description, value) {
@@ -89,6 +102,20 @@ var budgetController = (function() {
 			} else {
 				data.percentage = -1;
 			}
+		},
+
+		calculatePercentages: function(){
+			data.allItems.exp.forEach(function(cur){
+				cur.calcPercentage(data.totals.inc);
+			});
+		},
+
+		getPercentages: function(){
+			var allPerc = data.allItems.exp.map(function(cur){
+				return cur.getPercentage();
+			});
+
+			return allPerc;
 		},
 
 		getBudget: function(){
@@ -234,6 +261,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 			UICtrl.deleteListItem(itemID);
 
 			updateBudget();
+			updatePercentages();
 
 		}
 	};
@@ -245,6 +273,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 		var budget = budgetCtrl.getBudget();
 		//3. Display the budget on the UI
 		UICtrl.displayBudget(budget)
+	};
+
+	var updatePercentages = function(){
+		budgetCtrl.calculatePercentages();
+
+		var percentages = budgetCtrl.getPercentages();
+
+		console.log(percentages);
 	};
 
 	var ctrlAddItem = function() {
@@ -259,6 +295,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 			UICtrl.clearFields();
 		
 			updateBudget();
+			updatePercentages();
 		}
 	};
 
