7d0be5725ed095124b9a3c109028b0965a2d4237
Tomasz Gajda <tomasz_gajda@outlook.com>
2020-07-30 21:41:40 +0200

    added missing '%' sign

--- app.js
+++ app.js
@@ -236,7 +236,7 @@ var UIController = (function() {
 			document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
 
 			if(obj.percentage > 0) {
-				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
 			} else {
 				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
 			}
