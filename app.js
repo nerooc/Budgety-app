ce8175fe76345f985a9e176c2144d019725058f8
Tomasz Gajda <tomasz_gajda@outlook.com>
2020-07-30 16:10:37 +0200

    UI colors changing depending on type

--- app.js
+++ app.js
@@ -169,6 +169,12 @@ var UIController = (function() {
 		
 		return (type === 'exp' ?  '-' : '+') + ' ' + int + '.' + dec;
 	};
+
+	var nodeListForEach = function(list, callback){
+		for(var i = 0; i < list.length; i++){
+			callback(list[i], i);
+		}
+	};
 	
 	return {
 		getInput: function() {
@@ -241,17 +247,11 @@ var UIController = (function() {
 		},
 
 		displayPercentages: function(percentages){
-			var fields, nodeListForEach;
+			var fields;
 			
 			fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-			nodeListForEach = function(list, callback){
-				for(var i = 0; i < list.length; i++){
-					callback(list[i], i);
-				}
-		};
-
-		nodeListForEach(fields, function(current, index){
+			nodeListForEach(fields, function(current, index){
 				if(percentages[index] > 0){
 					current.textContent = percentages[index] + '%';
 				} else {
@@ -275,6 +275,23 @@ var UIController = (function() {
 			
 		},
 
+		changedType: function(event) {
+			var fields;
+
+			fields = document.querySelectorAll(
+				DOMstrings.inputType + ',' +
+				DOMstrings.inputDescription + ',' +
+				DOMstrings.inputValue
+			);
+
+			nodeListForEach(fields, function(cur){
+				cur.classList.toggle('red-focus');
+			});
+
+			document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		},
@@ -295,6 +312,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 		});
 
 		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+		document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
 	};
 
